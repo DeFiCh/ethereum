3bda724b6afbe173a2312c7af3cb8cf5730b98cc
Prasanna Loganathar <pvl@prasannavl.com>
2023-10-09 22:35:11 +0800

    Add global cache

--- src/header.rs
+++ src/header.rs
@@ -1,17 +1,29 @@
 use ethereum_types::{Bloom, H160, H256, H64, U256};
 use sha3::{Digest, Keccak256};
-use std::cell::Cell;
-use rlp::{Encodable, Decodable};
+use lru::LruCache;
+use spin::Mutex;
+use std::sync::OnceLock;
 
 use crate::Bytes;
 
+fn header_hash_cache() -> &'static Mutex<lru::LruCache<Vec<u8>, H256>> {
+	pub static CACHE: OnceLock<Mutex<lru::LruCache<Vec<u8>, H256>>> = OnceLock::new();
+	CACHE.get_or_init(|| {
+		let cache_size: std::num::NonZeroUsize = std::num::NonZeroUsize::new(1024 * 10).unwrap();
+		Mutex::new(LruCache::new(cache_size))
+	})
+}
+
+
 #[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(rlp::RlpEncodable, rlp::RlpDecodable)]
 #[cfg_attr(
 	feature = "with-codec",
 	derive(codec::Encode, codec::Decode, scale_info::TypeInfo)
 )]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 /// Ethereum header definition.
+
 pub struct Header {
 	pub parent_hash: H256,
 	pub ommers_hash: H256,
@@ -29,8 +41,6 @@ pub struct Header {
 	pub mix_hash: H256,
 	pub nonce: H64,
 	pub base_fee: U256,
-	#[cfg_attr(feature = "with-serde", serde(skip))]
-	hash_cache: Cell<Option<H256>>,
 }
 
 impl Header {
@@ -53,66 +63,15 @@ impl Header {
 			mix_hash: partial_header.mix_hash,
 			nonce: partial_header.nonce,
 			base_fee: partial_header.base_fee,
-			hash_cache: Cell::new(None),
 		}
 	}
 
 	#[must_use]
 	pub fn hash(&mut self) -> H256 {
-		let h = &self.hash_cache;
-		if h.get().is_none() {
-			let val = H256::from_slice(Keccak256::digest(&rlp::encode(self)).as_slice());
-			h.set(Some(val));
-		}
-		h.get().unwrap()
-	}
-}
-
-
-impl Encodable for Header {
-	fn rlp_append(&self, s: &mut rlp::RlpStream) {
-		s
-			.begin_list(16)
-			.append(&self.parent_hash)
-			.append(&self.ommers_hash)
-			.append(&self.beneficiary)
-			.append(&self.state_root)
-			.append(&self.transactions_root)
-			.append(&self.receipts_root)
-			.append(&self.logs_bloom)
-			.append(&self.difficulty)
-			.append(&self.number)
-			.append(&self.gas_limit)
-			.append(&self.gas_used)
-			.append(&self.timestamp)
-			.append(&self.extra_data)
-			.append(&self.mix_hash)
-			.append(&self.nonce)
-			.append(&self.base_fee);
-	}
-}
-
-impl Decodable for Header {
-	fn decode(rlp: &rlp::Rlp<'_>) -> Result<Self, rlp::DecoderError> {
-		Ok(Header {
-			parent_hash: rlp.val_at(0)?,
-			ommers_hash: rlp.val_at(1)?,
-			beneficiary: rlp.val_at(2)?,
-			state_root: rlp.val_at(3)?,
-			transactions_root: rlp.val_at(4)?,
-			receipts_root: rlp.val_at(5)?,
-			logs_bloom: rlp.val_at(6)?,
-			difficulty: rlp.val_at(7)?,
-			number: rlp.val_at(8)?,
-			gas_limit: rlp.val_at(9)?,
-			gas_used: rlp.val_at(10)?,
-			timestamp: rlp.val_at(11)?,
-			extra_data: rlp.val_at(12)?,
-			mix_hash: rlp.val_at(13)?,
-			nonce: rlp.val_at(14)?,
-			base_fee: rlp.val_at(15)?,
-			hash_cache: Cell::new(None),
-		})
+		let rlp_encoded = &rlp::encode(self);
+		header_hash_cache().lock().get_or_insert(rlp_encoded.to_vec(), move || {
+			H256::from_slice(Keccak256::digest(rlp_encoded).as_slice())
+		}).clone()
 	}
 }
 
